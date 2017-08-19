ed6e619ac5aa3aaffb17e85b94f336f976246e9e
Chien <chien@gmail.com>
2017-08-19 16:03:52 +0800

    v2

--- app/src/main/java/tw/brad/app/helloworld/myonbootservice/MyService.java
+++ app/src/main/java/tw/brad/app/helloworld/myonbootservice/MyService.java
@@ -2,11 +2,13 @@ package tw.brad.app.helloworld.myonbootservice;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 
 import java.util.Timer;
@@ -49,10 +51,21 @@ public class MyService extends Service {
         builder.setContentText("Content");
         builder.setAutoCancel(true);
 
+        Intent it = new Intent(this, NoticeActivity.class);
+        it.putExtra("key", i);
+
+        TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(this);
+        taskStackBuilder.addParentStack(NoticeActivity.class);
+        taskStackBuilder.addNextIntent(it);
+
+        PendingIntent pendingIntent =
+                taskStackBuilder.getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        builder.setContentIntent(pendingIntent);
 
         Notification notification = builder.build();
 
-        nmgr.notify(i,notification);
+        nmgr.notify(1,notification);
 
     }
 
